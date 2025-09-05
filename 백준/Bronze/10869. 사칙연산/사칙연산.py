4b31b61b4abac4b455ba9d774ed2463cf1c3fa1e
RedHong56 <redhong9956@gmail.com>
2025-09-05 15:11:32 +0900

    Update 사칙연산.py

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,6 +1,22 @@
-A, B = map(int, input().split())
-print(A+B)
-print(A-B)
-print(A*B)
-print(A//B)
-print(A%B)
+# A, B = map(int, input().split())
+# print(A+B)
+# print(A-B)
+# print(A*B)
+# print(A//B)
+# print(A%B)
+
+
+# map() 각 원소를 변환하는 기능
+C, D = map(int, input()) #input: 35
+print(C) # 3
+print(D) # 5
+print(type(C))
+print(type(D))
+
+#.split() 문자열을 토막내서 리스트로 만드는 기능
+text = "apple banana cherry"
+print(text)
+testsplit = text.split()   # 공백 기준으로 자르기
+print(testsplit)
+print(text.split())
+
