1145df5b28c32f532c525c01434ab9a0bcec8d89
RedHong56 <redhong9956@gmail.com>
2025-09-25 21:32:02 +0900

    DP Top, Bottom 추가

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -14,3 +14,35 @@ def fivonacci(a):
 
 
 print(fivonacci(n))
+
+
+#--------------------------------
+#DP Top- down
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    arr = [0] * (n+1)
+    arr[1] = 1
+    for i in range(2, n+1):
+        arr[i] = arr[i-1] + arr[i-2]
+    return arr[n]
+
+n = int(input())
+print(fibonacci(n))
+#----------------------------------
+#DP Bottom-up
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    prev, curr = 0, 1  # arr[0]=0, arr[1]=1
+    for _ in range(2, n + 1):
+        prev, curr = curr, prev + curr
+    return curr
+
+n = int(input())
+print(fibonacci(n))
