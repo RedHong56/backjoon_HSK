e97ed024102ca12febc9a6f927dce0aaacbba991
홍순규 <156219572+RedHong56@users.noreply.github.com>
2026-04-11 21:24:38 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,9 @@
+receipt = int(input())
+n = int(input())
+for i in range(0,n):
+    price, num = map(int, input().split())
+    receipt -= price * num
+if  receipt == 0 :
+    print("Yes")
+else:
+    print("No")
