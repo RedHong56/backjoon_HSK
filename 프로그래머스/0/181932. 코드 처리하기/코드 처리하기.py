8c599e5e6547e88aaa0b421bcd0fff195d30a323
홍순규 <156219572+RedHong56@users.noreply.github.com>
2026-04-23 14:09:59 +0900

    [level 0] Title: 코드 처리하기, Time: 15.43 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,14 @@
+def solution(code):
+    answer = ""
+    mode = False
+    
+    for i in range(len(code)):
+        if code[i] == "1":
+            mode = not mode
+        else:
+            if mode and i % 2 == 1:
+                answer += code[i]
+            elif not mode and i % 2 == 0:
+                answer += code[i]
+                
+    return answer if answer else "EMPTY"
