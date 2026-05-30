45003e85cbc53fe8c1d1cb20cc8d616d67793bd2
RedHong56 <redhong9956@gmail.com>
2026-05-30 18:04:21 +0900

    몸풀기 문제 백트레킹: 합이 10되는 부분 집합

---
+++ Warm_up/47_backTracking_self.cpp
@@ -0,0 +1,82 @@
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> results;
+vector<int> selected_nums;
+// 합이 10이 되는
+void backtrack(int N, int sum, int start) {
+  if(sum == 10) {
+    results.push_back(selected_nums);
+    return;
+  }
+  for (int i = start; i < N; i++) {
+    if (sum + i <= 10) { // 더해야 10이기에 재귀 해야 result에 들어감
+      selected_nums.push_back(i);
+      backtrack(N, sum + i, i + 1);
+      selected_nums.pop_back();
+    }
+    
+  }
+  
+  
+}
+
+vector<vector<int>> solution(int N) {
+  //❸ 숫자 1부터 백트래킹 시작
+  backtrack(N, 0, 1);
+  return results;
+}
+
+
+
+//아래 코드는 테스트 코드 입니다.
+#include <iterator>
+#include <iostream>
+
+using namespace std;
+
+void init()
+{
+  results.clear();
+  selected_nums.clear();    
+}
+
+void print(vector<vector<int>> vec)
+{
+    
+  for(int i = 0; i < vec.size(); i++ ){
+    copy(vec[i].begin(), vec[i].end(), std::ostream_iterator<int>(cout, " "));
+    cout << endl;
+  }
+    
+}
+
+int main()
+{
+  print(solution(5));
+  init();
+  /*
+    출력값
+    1 2 3 4 
+    1 4 5 
+    2 3 5 
+  */
+  print(solution(2));
+  init();
+  /*
+    출력값(없음)
+  */  
+  print(solution(7));
+  /*
+    출력값
+    1 2 3 4 
+    1 2 7 
+    1 3 6 
+    1 4 5 
+    2 3 5 
+    3 7 
+    4 6 
+  */
+  return 0;
+}
